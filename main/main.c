d1ce3ec5e9cdf6b3ffb326b3dfd1c00e5e8151b9
Adrien Martinez <adrien.m.j@opendeusto.es>
2025-04-23 12:10:12 +0200

    main cambiado

--- main/main.c
+++ main/main.c
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include "../menu/menus.h"
 
-int main(void) {
-    /*while (menuInicioSesion()) {  // Mientras el usuario inicie sesión, entra a MENU2
-        menuOpciones();
-    }*/
-   menuInicioSesion();
-=======
 #include "../menu/menus.h"
 
 int main(void) {
     menuInicioSesion();
->>>>>>> 7a1f47f350a88ce808f4c13bae936ada2aad25a5
     return 0;
 }
 
